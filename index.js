2be9cc866f690cb01485bd3f6cf08ca7854eb093
Vinay <vinaytiwari1501@gmail.com>
2022-12-24 11:41:17 +0530

    First Action

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('From index.js')
+const BUY_CAKE = 'BUY_CAKE'
+
+
+//action
+{
+    type : BUY_CAKE,
+    info : 'First rdux Action'
+}
+
+
+//action creator
+function buyCake(){
+    return{
+        type : BUY_CAKE,
+        infor : 'First redux action'
+    }
+}
