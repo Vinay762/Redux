02ca43a4e4e10e3af36cb2d7387205ace53413d1
Vinay <vinaytiwari1501@gmail.com>
2022-12-24 13:02:06 +0530

    Middleware

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const redux = require('redux')
-
+const reduxLogger = require('redux-logger')
 const createStore = redux.createStore
 const combineReducers = redux.combineReducers
+const applyMiddleware = redux.applyMiddleware
+const logger = reduxLogger.createLogger()
 
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICECREAMS = 'BUY_ICECREAMS'
@@ -81,9 +83,9 @@ const rootReducer = combineReducers({
     cake : cakeReducer,
     iceCream : iceCreamReducer
 })
-const store = createStore(rootReducer)
+const store = createStore(rootReducer, applyMiddleware(logger))
 console.log('Initial State', store.getState());
-const unsubscribe = store.subscribe(() => console.log('upated State', store.getState()));
+const unsubscribe = store.subscribe(() => {});
 store.dispatch(buyCake());
 store.dispatch(buyCake());
 store.dispatch(buyCake());
