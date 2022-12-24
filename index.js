a2e2bc11727be48ed47a9024ff1f42276802b9e4
Vinay <vinaytiwari1501@gmail.com>
2022-12-24 11:47:08 +0530

    Reducer

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const BUY_CAKE = 'BUY_CAKE'
 
 
 //action
-{
-    type : BUY_CAKE,
-    info : 'First rdux Action'
-}
+// {
+//     type : BUY_CAKE,
+//     info : 'First rdux Action'
+// }
 
 
 //action creator
@@ -15,3 +15,22 @@ function buyCake(){
         infor : 'First redux action'
     }
 }
+
+
+//reducers (previous, action) => newState
+
+const initialState = {
+    numOfCakes : 10
+}
+
+const reducer = (state = initialState, action) => {
+    switch(action.type){
+        case BUY_CAKE : return {
+            ...state,
+            numOfCakes : state.numOfCakes-1
+        }
+        default : return state
+    }
+}
+
+
