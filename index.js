8429016a5a5b6d9b838dd663f7aba24ce2c2b47a
Vinay <vinaytiwari1501@gmail.com>
2022-12-24 12:43:48 +0530

    Multiple Reducers

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const redux = require('redux')
 
 const createStore = redux.createStore
+const combineReducers = redux.combineReducers
 
 const BUY_CAKE = 'BUY_CAKE'
 const BUY_ICECREAMS = 'BUY_ICECREAMS'
@@ -17,7 +18,6 @@ const BUY_ICECREAMS = 'BUY_ICECREAMS'
 function buyCake(){
     return{
         type : BUY_CAKE,
-        infor : 'First redux action'
     }
 }
 
@@ -29,17 +29,46 @@ function buyIcecream(){
 
 //reducers (previous, action) => newState
 
-const initialState = {
-    numOfCakes : 10, 
-    numOfIceCreams : 20
+// const initialState = {
+//     numOfCakes : 10, 
+//     numOfIceCreams : 20
+// }
+
+const initialCakeState = {
+    noOfCakes : 10,
 }
 
-const reducer = (state = initialState, action) => {
+const initialIceCreamState = {
+    noOfIceCreams : 20
+}
+
+// const reducer = (state = initialState, action) => {
+//     switch(action.type){
+//         case BUY_CAKE : return {
+//             ...state,
+//             numOfCakes : state.numOfCakes-1
+//         }
+//         case BUY_ICECREAMS : return {
+//             ...state,
+//             numOfIceCreams : state.numOfIceCreams-1
+//         }
+//         default : return state
+//     }
+// }
+
+const cakeReducer = (state = initialCakeState, action) => {
     switch(action.type){
         case BUY_CAKE : return {
             ...state,
             numOfCakes : state.numOfCakes-1
         }
+        
+        default : return state
+    }
+}
+
+const iceCreamReducer = (state = initialIceCreamState, action) => {
+    switch(action.type){
         case BUY_ICECREAMS : return {
             ...state,
             numOfIceCreams : state.numOfIceCreams-1
@@ -48,9 +77,11 @@ const reducer = (state = initialState, action) => {
     }
 }
 
-
-
-const store = createStore(reducer)
+const rootReducer = combineReducers({
+    cake : cakeReducer,
+    iceCream : iceCreamReducer
+})
+const store = createStore(rootReducer)
 console.log('Initial State', store.getState());
 const unsubscribe = store.subscribe(() => console.log('upated State', store.getState()));
 store.dispatch(buyCake());
